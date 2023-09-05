708c244b401a523ce00e59bf765b41b4be74be86
Peter Clift <petercli@hotmail.com>
2023-09-05 05:12:06 -0700

    first push

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # DataRecovery
-recover important files
+Powershell script to recover important files from  Windows Drives
+
+Edit variables , like $drive="f:\"
+
+First, take ownership of drive
+RK drive/properties/security/advanced/change permission
+Owner/change pcmain/peter  (from whoami)
+
---
+++ scanhd.ps1
@@ -0,0 +1,51 @@
+# D:\FileRecovery\scanhd.ps1
+# 
+#
+# to run. Powershell command :  .\scanhd.ps1
+#
+
+
+
+$drive="f:\"
+$magicWord1 = "windows"
+$magicWord2 = "microsoft"
+$magicWord3 = "program files"
+$codePath="D:\FileRecovery\"
+$tempPath="D:\FileRecovery\tmp\"
+############################################
+try{
+
+    #throw "Test Error"
+
+    Remove-Item  $tempPath -Force  -Recurse -ErrorAction SilentlyContinue
+    New-Item -Path $codePath -Name "tmp" -ItemType "directory"
+
+
+    $files=   Get-ChildItem  $drive    -include ('*.wav','*.doc', '*.docx', '*.mp4', '*.pic', '*.jpg', '*.bmp'  , '*.avi', '*.mp3', '*.mpg', '*.wmv'    ) -recurse
+
+
+    Write-Host ("Number of entries found:"+ $files.length)
+    Write-Host "after" 
+
+    foreach ($file in $files)
+    {
+    
+        
+        if($file.FullName -match $magicWord1  -or $file.FullName -match $magicWord2 -or $file.FullName -match $magicWord3)
+        {
+            Write-Output ("Exclude :" + $file.FullName)
+            continue
+        }
+
+        Write-Output ("Include file name:" + $file.FullName)
+        Copy-Item -Path $file.FullName -Destination $tempPath
+    }
+
+}
+catch{
+    Write-Host "An error occurred:"+$error[0]
+}
+  
+finally{
+    Write-Host "Finished."
+}
